37a3717db3357489d70877640907f366263f4b71
Trupoed <100982669+trupo-ed@users.noreply.github.com>
2022-04-02 12:02:21 +0600

    Add files via upload

---
+++ HomePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Snegirev_PR15_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click (object sender, RoutedEventArgs e)
+        {
+            ReportPage r = new ReportPage(this.lyudi.SelectedItem);
+            this.NavigationService.Navigate(r);
+        }
+    }
+}
---
+++ ReportPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Snegirev_PR15_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для ReportPage.xaml
+    /// </summary>
+    public partial class ReportPage : Page
+    {
+        public ReportPage()
+        {
+            InitializeComponent();
+        }
+        public ReportPage(object data) : this()
+        {
+            this.DataContext = data;
+        }
+    }
+}
